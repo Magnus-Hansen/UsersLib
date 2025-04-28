1936586f01ef5e3080bd77a44bce86689fa7e288
Magnus Ansbjerg Selchou Hansen <mah012@edu.zealand.dk>
2025-04-28 11:45:56 +0200

    Add project files.

---
+++ UsersLib/User.cs
@@ -0,0 +1,31 @@
+﻿namespace UsersLib
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+
+        public User(string name, string password)
+        {
+            
+        }
+        public void ValidateName()
+        {
+            if (Name == null) throw new ArgumentNullException("Name can't be null");
+        }
+        public void ValidatePassword()
+        {
+            if (Password == null) throw new ArgumentNullException("Password can't be null");
+        }
+        public void Validate()
+        {
+            ValidateName();
+            ValidatePassword();
+        }
+        public override string ToString()
+        {
+            return $"Id: {Id} -- Name: {Name} -- Password: {Password}";
+        }
+    }
+}
---
+++ UsersLib/UserRepo.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsersLib
+{
+    public class UserRepo
+    {
+        private List<User> _users { get; set; } = new List<User>();
+        private int _nextId { get; set; }
+
+        public UserRepo()
+        {
+            
+        }
+
+        public List<User> GetAll()
+        {
+            return new List<User>(_users);
+        }
+
+        public User Get(int id)
+        {
+            User? user = _users.Find(p => p.Id == id);
+            if (user != null)
+            {
+                return user;
+            }
+            return null;
+        }
+
+        public User Add(User user)
+        {
+            user.Validate();
+            _nextId++;
+            user.Id = _nextId;
+            _users.Add(user);
+            return user;
+        }
+
+        public User Delete(int id)
+        {
+            if (Get(id) != null)
+            {
+                User user = Get(id);
+
+                _users.Remove(Get(id));
+                return user;
+            }
+            return null;
+        }
+    }
+}
